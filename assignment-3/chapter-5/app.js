6633f2bac1fb50a361fe7e00041129df3f59deef
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-02-04 03:17:47 +0500

    added index and js files

---
+++ assignment-3/chapter-5/app.js
@@ -0,0 +1,248 @@
+//***** CHAPTER 5****//
+
+//Question 1//
+
+//var numone=3
+//var numtwo=5
+//newnum=numone+numtwo
+//console.log(newnum);
+//document.write("Sum of 3 and 5 is 8")
+
+//Question 2
+
+//var numone=5
+//var numtwo=3
+//newnum=numone-numtwo
+//console.log(newnum)
+//document.write("Subtraction of 5 and 3 is 2")
+
+//var numone=5
+//var numtwo=3
+//newnum=numone*numtwo
+//console.log(newnum)
+//document.write("Multiplication of 5 and 3 is 15")
+
+//var numone=5
+//var numtwo=3
+//newnum=numone%numtwo
+//console.log(newnum)
+//document.write("modulus of 5 and 3 is 2")
+
+//var numone=5
+//var numtwo=5
+//newnum=numone/numtwo
+//console.log(newnum)
+//document.write("division of 5 and 5 is 1")
+
+//Question 3
+
+//var numone=5+7
+//console.log(numone)
+//document.write("Value after variable declaration is Undefined")
+//document.write("<br>")
+//document.write("Initial value:5")
+//newnum=++numone
+//console.log(newnum)
+//document.write("<br>")
+//document.write("Value after increment is: 6")
+//document.write("<br>")
+//document.write("Value after increment is: 13")
+//anothernum=--newnum
+//console.log(anothernum)
+//document.write("<br>")
+//document.write("Value after decrement is: 12")
+//totalnum=anothernum/3
+//console.log(totalnum)
+//document.write("<br>")
+//document.write("The remainder is: 0")
+
+//Question 4
+
+//var costOfOneMovieTicket=600
+//totalCostOfMovieTiket=costOfOneMovieTicket*5
+//console.log(totalCostOfMovieTiket)
+//document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+//Question 5
+
+//var numone=4*1
+//var numtwo=4*2
+//var numthree=4*3
+//var numfour=4*4
+//var numfive=4*5
+//var numsix=4*6
+//var numseven=4*7
+//var numeight=4*8
+//var numnine=4*9
+//var numten=4*10
+//console.log(numone)
+//console.log(numtwo)
+//console.log(numthree)
+//console.log(numfour)
+//console.log(numfive)
+//console.log(numsix)
+//console.log(numseven)
+//console.log(numeight)
+//console.log(numnine)
+//console.log(numten)
+//document.write("Table of 4")
+//document.write("<br>")
+//document.write("4x1=4")
+//document.write("<br>")
+//document.write("4x2=8")
+//document.write("<br>")
+//document.write("4x3=12")
+//document.write("<br>")
+//document.write("4x4=16")
+//document.write("<br>")
+//document.write("4x5=20")
+//document.write("<br>")
+//document.write("4x6=24")
+//document.write("<br>")
+//document.write("4x7=28")
+//document.write("<br>")
+//document.write("4x8=32")
+//document.write("<br>")
+//document.write("4x9=36")
+//document.write("<br>")
+//document.write("4x10=40")
+
+//Question 6
+
+//var inCelsius=50
+//var inFahrenhiet=(inCelsius*9/5)+32;
+//console.log(inFahrenhiet);
+//str="\u00B0"
+//document.write("50"+str+"C is 122"+str+"f")
+
+//var inFahrenhiet=80
+//var inCelsius=(inFahrenhiet-32)*5/9;
+//console.log(inCelsius);
+//str="\u00B0"
+//document.write("<br>")
+//document.write("80"+str+"f is 26.66"+str+"C")
+
+//Question 7
+
+//var heading="Shopping Cart";
+//document.write(heading.bold(heading));
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//var priceOfItem1=650
+//var priceOfItem2=100
+//var quantityOfItem1=3
+//var quantityOfItem2=7
+//var shippingCharges=100
+//var totalOfItem1=650*3
+//var totalOfItem2=100*7
+//var totalCost= totalOfItem1+ totalOfItem2+shippingCharges;
+//console.log(totalCost);
+//document.write("Price of item 1 is 650")
+//document.write("<br>")
+//document.write("Quantity of item 1 is 3")
+//document.write("<br>")
+//document.write("Price of item 2 is 100")
+//document.write("<br>")
+//document.write("Quantity of item 2 is 7")
+//document.write("<br>")
+//document.write("Shipping charges 100")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("Total cost of your order is 2750")
+
+//Question 8
+
+//var totalMarks=980
+//var marksObtained=804
+//percentage=marksObtained/totalMarks*100;
+//console.log(percentage);
+//var heading="Marks Sheet"
+//document.write(heading.bold(heading))
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("Total marks:"+totalMarks)
+//document.write("<br>")
+//document.write("Marks obtained:"+marksObtained)
+//document.write("<br>")
+//document.write("Percentage:"+percentage)
+
+//Question 9
+
+//var quantityOfUSDollars=10
+//var exchangeRateOf1Dollar=265.82
+//var inPKR=quantityOfUSDollars*exchangeRateOf1Dollar;
+//console.log(inPKR);
+//var quantityOfSaudiRiyals=25
+//var exchangeRateOf1SaudiRiyal=70.83
+//var inPKR=quantityOfSaudiRiyals*exchangeRateOf1SaudiRiyal;
+//console.log(inPKR)
+//var heading="Currency in PKR"
+//document.write(heading.bold(heading))
+//document.write("<br>")
+//document.write("<br>")
+//document.write("Total currency in PKR:1748")
+
+//Question 10
+
+//var numone=10
+//newnum=(numone+5)+numone*10+115/2;
+//console.log(newnum)
+
+//Question 11
+
+//var currentYear=2023
+//var birthYear=1996
+//var age=currentYear-birthYear
+//console.log(age)
+//var heading="Age Calculator"
+//document.write(heading.bold(heading))
+//document.write("<br>")
+//document.write("<br>")
+//document.write("Current Year:"+currentYear)
+//document.write("<br>")
+//document.write("Birth year:"+birthYear)
+//document.write("<br>")
+//document.write("Your age is:"+age)
+
+//Question 12
+
+//var radiusOfCircle=30
+//circumference=2*3.142*30
+//console.log(circumference);
+//Area=3.142*900
+//console.log(Area)
+//var heading="The Geometrizer"
+//document.write(heading.bold(heading))
+//document.write("<br>")
+//document.write("<br>")
+//document.write("radius of a circle:"+radiusOfCircle)
+//document.write("<br>")
+//document.write("The circumference is:"+circumference)
+//document.write("<br>")
+//document.write("The area is:"+Area)
+
+//Question 13
+
+//var favouriteSnack="Magnum"
+//var currentAge=15
+//var maximumAge=65
+//var snacksPerDay=3
+//var totalSnacks=(maximumAge-currentAge)*snacksPerDay
+//console.log(totalSnacks)
+//var heading="The Lifetime Supply Calculator"
+//document.write(heading.bold(heading))
+//document.write("<br>")
+//document.write("<br>")
+//document.write("Favourite snack:"+favouriteSnack)
+//document.write("<br>")
+//document.write("Current Age:"+currentAge)
+//document.write("<br>")
+//document.write("Estimated maximum age:"+maximumAge)
+//document.write("<br>")
+//document.write("Amount of snacks per day:"+snacksPerDay)
+//document.write("<br>")
+//document.write("You will need 150 magnum to last you until the ripe old age of 65")
---
+++ assignment-3/chapter-6/app.js
@@ -0,0 +1,139 @@
+//***** Chapter-6 ****//
+
+//Question1
+
+//var num=6
+//console.log(num)
+//document.write("Result:")
+//document.write("<br>")
+//document.write("The value of num is 6")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//num1=++num
+//console.log(num1)
+//document.write("The value of ++num is:7")
+//document.write("<br>")
+//document.write("Now the value of num is 7")
+//document.write("<br>")
+//document.write("<br>")
+//num2=num1++
+//console.log(num2)
+//document.write("<br>")
+//document.write("<br>")
+//document.write("The value of num++ is 6")
+//document.write("<br>")
+//document.write("Now the value of num1++ is 8")
+//num3=--num2
+//console.log(num3)
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("The value of --num is 6")
+//document.write("<br>")
+//document.write("Now the value of --num is 6")
+//num4=num2--
+//console.log(num4)
+//document.write("<br>")
+//document.write("<br>")
+//document.write("<br>")
+//document.write("The value of num-- is 6")
+//document.write("<br>")
+//document.write("Now the value of num is 5")
+
+// Question 2
+
+//var a=2
+//var b=1
+//num1=--a
+//num2=--b
+//num3=++b
+//num4=b--
+//num1-num2
+//num1-num2+num3
+//totalNum=num1-num2+num3+num4
+//console.log(totalNum)
+//document.write("a is 2")
+//document.write("<br>")
+//document.write("b is 1")
+//document.write("<br>")
+//document.write("result is 3")
+
+//Question 3
+
+//var fullname=prompt("Enter your name")
+//console.log(fullname)
+//var greet=prompt("Enter greetings")
+//console.log(greet)
+
+//Question 4
+
+// var num=prompt("Enter a number")
+// //console.log(num)
+// document.write("3x1=3")
+// document.write("<br>")
+// document.write("3x2=6")
+// document.write("<br>")
+// document.write("3x3=9")
+// document.write("<br>")
+// document.write("3x4=12")
+// document.write("<br>")
+// document.write("3x5=15")
+// document.write("<br>")
+// document.write("3x6=18")
+// document.write("<br>")
+// document.write("3x7=21")
+// document.write("<br>")
+// document.write("3x8=24")
+// document.write("<br>")
+// document.write("3x9=27")
+// document.write("<br>")
+// document.write("3x10=30")
+// var newnum=prompt("enter your num",5)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("5x1=5")
+// document.write("<br>")
+// document.write("5x2=10")
+// document.write("<br>")
+// document.write("5x3=15")
+// document.write("<br>")
+// document.write("5x4=20")
+// document.write("<br>")
+// document.write("5x5=25")
+// document.write("<br>")
+// document.write("5x6=30")
+// document.write("<br>")
+// document.write("5x7=35")
+// document.write("<br>")
+// document.write("5x8=40")
+// document.write("<br>")
+// document.write("5x9=45")
+// document.write("<br>")
+// document.write("5x10=50")
+// console.log(newnum)
+
+//Question 5
+
+// var firstSubject="Maths"
+// var secondSubject="Physics"
+// var thirdSubject="Chemistry"
+// var totalMarks=300
+// var obtainedMarksForFirstSubject=prompt("Enter marks")
+// //console.log(obtainedMarksForFirstSubject)
+// var obtainedMarksForSecondSubject=prompt("Enter marks")
+// //console.log(obtainedMarksForSecondSubject)
+// var obtainedMarksForthirdSubject=prompt("Enter marks")
+// //console.log(obtainedMarksForthirdSubject)
+// var obtainedMarks=54+39+25
+// //console.log(obtainedMarks)
+// var percentage1=(obtainedMarksForFirstSubject/100)*100
+// console.log(percentage1)
+// var percentage2=(obtainedMarksForSecondSubject/100)*100
+// console.log(percentage2)
+// var percentage3=(obtainedMarksForthirdSubject/100)*100
+// console.log(percentage3)
+// var totalPercentage=percentage1+percentage2+percentage3
+// console.log(totalPercentage/3)
